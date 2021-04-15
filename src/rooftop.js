fe67f03b8601d68df1c27c367521b6d0c4cf4021
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-04-15 11:07:10 -0500

    Added data to constructor function

---
+++ src/rooftop.js
@@ -0,0 +1,13 @@
+class Rooftop {
+
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.address = data.address;
+    this.image_url = data.image_url;
+    this.website_url = data.website_url;
+    this.neighborhood_id = data.neighborhood_id;
+    this.user_id = data.user_id 
+  }
+
+}
